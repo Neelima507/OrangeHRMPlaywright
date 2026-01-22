7d276936aa32ac8579268de23257c35384aba190
Neelima <neelutgd507@gmail.com>
2026-01-21 19:33:02 -0800

    added endtoend file

---
+++ tests/endtoendTest.spec.js
@@ -0,0 +1,14 @@
+import { test, expect } from "playwright/test"
+test('@Drops dynamic', async ({ page }) => {
+    await page.goto("https://www.saucedemo.com/");
+    await page.getByPlaceholder('Username').fill('standard_user');
+    await page.getByPlaceholder('Password').fill('secret_sauce');
+    await page.locator('#login-button').click();
+    await page.on('dialog', async dialog => {
+        console.log(dialog.message());
+        await dialog.accept(); // or dialog.dismiss()
+        const locator = page.getByText('Swag Labs')
+        console.log('inner text is ', await locator.innerText());
+
+    });
+})
